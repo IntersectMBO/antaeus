2170954db47b60180ebd2d15c872e3459db7300e
zeme <zem1@pm.me>
2023-05-03 12:32:05 +0700

    WIP

--- nix/shell-module.nix
+++ nix/shell-module.nix
@@ -1,5 +1,5 @@
 # Your development shell is defined here.
-# You can add packages, custom scripts and a shell hook.
+# You can add packages, scripts, envvars, and a shell hook.
 
 {
   # Desystemized merged inputs.
@@ -15,19 +15,25 @@
   # iogx and your `flake.nix`.
 , systemized-inputs
 
-  # The very config passed as second argument to `inputs.iogx.mkFlake` in your 
-  # `flake.nix`.
+  # The very attrset passed to `inputs.iogx.mkFlake` in your `flake.nix`.
 , flakeopts
 
   # Desystemized legacy nix packages configured against `haskell.nix`.
   # NEVER use the `nixpkgs` coming from `inputs` or `systemized-inputs`!
 , pkgs
+
+  # A reference to the `haskell.nix` project on top of which this shell will be 
+  # built. This can be used for example to bring some haskell executables into 
+  # the shell:
+  # packages = [
+  #   haskell-nix-project.hsPkgs.cardano-cli.components.exes.cardano-cli
+  #   haskell-nix-project.hsPkgs.cardano-node.components.exes.cardano-node
+  # ];
+  # Be careful not to refence the project's own haskell packages.
+, haskell-nix-project
 }:
 
-let
-  cardano = inputs.cardano-world.cardano.packages;
-in
 {
-  env.CARDANO_CLI = "${cardano.cardano-cli}/bin/cardano-cli";
-  env.CARDANO_NODE = "${cardano.cardano-node}/bin/cardano-node";
+  env.CARDANO_CLI = "${haskell-nix-project.hsPkgs.cardano-cli.components.exes.cardano-cli}/bin/cardano-cli";
+  env.CARDANO_NODE = "${haskell-nix-project.hsPkgs.cardano-node.components.exes.cardano-node}/bin/cardano-node";
 }
