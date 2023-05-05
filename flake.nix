275046b5299d9df0db39122f6954437e7093c46d
zeme <zem1@pm.me>
2023-05-05 17:58:05 +0700

    Fix bug

--- flake.nix
+++ flake.nix
@@ -8,9 +8,27 @@
   outputs = inputs:
     inputs.iogx.mkFlake {
       inherit inputs;
+      debug = true;
       repoRoot = ./.;
+      flakeOutputsPrefix = "";
+      systems = [ "x86_64-darwin" "x86_64-linux" ];
+      haskellCompilers = [ "ghc8107" ];
+      defaultHaskellCompiler = "ghc8107";
+      haskellCrossSystem = null;
+      haskellProjectFile = ./nix/haskell-project.nix;
+      perSystemOutputsFile = null;
       shellName = "antaeus";
       shellPrompt = "\n\\[\\033[1;31m\\][antaeus:\\w]\\$\\[\\033[0m\\] ";
+      shellWelcomeMessage = "🤟 \\033[1;31mWelcome to antaeus\\033[0m 🤟";
+      shellModuleFile = ./nix/shell-module.nix;
+      includeHydraJobs = true;
+      excludeProfiledHaskellFromHydraJobs = true;
+      blacklistedHydraJobs = [ ];
+      enableHydraPreCommitCheck = true;
+      includeReadTheDocsSite = false;
+      readTheDocsSiteDir = null;
+      readTheDocsHaddockPrologue = "";
+      readTheDocsExtraHaddockPackages = _: { };
     };
 
   nixConfig = {
