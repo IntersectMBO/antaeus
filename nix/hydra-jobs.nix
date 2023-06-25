981049aef8e67d977feceae8e827787e389b22af
zeme <lorenzo.calegari@iohk.io>
2023-06-25 19:30:49 +0200

    WIP

--- nix/hydra-jobs.nix
+++ nix/hydra-jobs.nix
@@ -7,6 +7,9 @@
   ];
 
   excludedPaths = [
+    "checks.ghc927-mingwW64"
+    "packages.ghc927-mingwW64"
+    "devShells.ghc927-mingwW64"
     "packages.ghc927-profiled"
     "devShells.ghc927-profiled"
     "checks.ghc927-profiled"
