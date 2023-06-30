52ab20568c5c8ae79562f5769ae0ee05890dafff
zeme <lorenzo.calegari@iohk.io>
2023-06-30 10:11:05 +0200

    Bump IOGX to bc5f588

--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,5 +1,7 @@
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
+
 {
-  repoRoot = ../.;
   systems = [ "x86_64-linux" "x86_64-darwin" ];
   haskellCompilers = [ "ghc927" ];
   shouldCrossCompile = false;
