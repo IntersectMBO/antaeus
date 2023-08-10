b9776d64afe29b50c982975b91946d9489f1994d
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-08-10 12:17:13 +0200

    Bump IOGX v3.0 (#38)

--- flake.nix
+++ flake.nix
@@ -1,24 +1,31 @@
+# This file is part of the IOGX template and is documented at the link below:
+# https://www.github.com/input-output-hk/iogx#31-flakenix
+
 {
   description = "End-to-End Testing Framework for Cardano Haskell APIs";
 
   inputs = {
     iogx.url = "github:input-output-hk/iogx";
-    iogx.inputs.hackage.follows = "hackage_2";
-    iogx.inputs.CHaP.follows = "CHaP_2";
+    iogx.inputs.hackage.follows = "hackage";
+    iogx.inputs.CHaP.follows = "CHaP";
 
-
-    hackage_2 = {
+    hackage = {
       url = "github:input-output-hk/hackage.nix";
       flake = false;
     };
 
-    CHaP_2 = {
+    CHaP = {
       url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
       flake = false;
     };
   };
 
-  outputs = inputs: inputs.iogx.lib.mkFlake inputs ./.;
+
+  outputs = inputs: inputs.iogx.lib.mkFlake {
+    inherit inputs;
+    repoRoot = ./.;
+    systems = [ "x86_64-darwin" "x86_64-linux" "aarch64-darwin" ];
+  };
 
 
   nixConfig = {
