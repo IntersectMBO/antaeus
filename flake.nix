c991d17f5605c962782f8ed80d68be8295577643
zeme <lorenzo.calegari@iohk.io>
2024-01-18 11:03:53 +0100

    Bump IOGX 2024-01-17

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     iogx = {
-      url = "github:input-output-hk/iogx?ref=custom-precommit-hooks";
+      url = "github:input-output-hk/iogx";
       inputs.hackage.follows = "hackage";
       inputs.CHaP.follows = "CHaP";
       inputs.haskell-nix.follows = "haskell-nix";
