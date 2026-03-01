d91633ac2d8a206c906d855846a70dfd985e2650
Lemayan Leleina <lemayanleleina@gmail.com>
2026-03-01 22:17:20 +0300

    Double txValidityUpperBound to 5400 slots to prevent mempool expiration on slow runners

--- e2e-tests/test/Spec/ConwayFeatures.hs
+++ e2e-tests/test/Spec/ConwayFeatures.hs
@@ -167,7 +167,7 @@ checkTxInfoV3Test networkOptions TestParams{..} = do
           , C.txOuts = [txOut1, txOut2]
           , C.txFee = Tx.txFee era fee
           , C.txValidityLowerBound = Tx.txValidityLowerBound era 1
-          , C.txValidityUpperBound = Tx.txValidityUpperBound era 2700
+          , C.txValidityUpperBound = Tx.txValidityUpperBound era 5400
           , -- \^ ~9min range (200ms slots)
             -- \^ Babbage era onwards cannot have upper slot beyond epoch boundary (10_000 slot epoch)
             C.txExtraKeyWits = Tx.txExtraKeyWits era [w1VKey]
